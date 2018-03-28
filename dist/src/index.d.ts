0731d690a6afbf9f27de06475c0d6641d8f68bb6
Dennis Teo <dynasdaq@gmail.com>
2018-03-28 08:06:15 +0200

    last minute changes

--- dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { DynaAutoComplete, IDynaAutoCompleteProps, IAutoCompleteValue, EMode, EStyle, EColor, ESize } from "./DynaAutoComplete";
-import { debounce } from "./utils";
-export { DynaAutoComplete, IDynaAutoCompleteProps, IAutoCompleteValue, EMode, EStyle, EColor, ESize, debounce };
